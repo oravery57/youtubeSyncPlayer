6f43895c8ba339d14c9c4ed4f667d13d2b3854e1
Hoge <hoge@hoge.com>
2017-02-03 17:29:11 +0900

    fix.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
+app.on('request', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});Å@
+  res.write('hello world');
+  res.end();
+});
+
 app.listen(server_port, server_ip_address, function () {
     console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
 });
