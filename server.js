f7b09876845761708af3f90b4aa6ed66a8e11066
Yuki Ochi <yuki.ochi.oravery@gmail.com>
2017-02-04 17:41:56 +0900

    fix.

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-app.on('request', function(req, res) {
+var server = http.createServer();
+server.on('request', function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('hello world');
   res.end();
 });
 
-app.listen(server_port, server_ip_address, function () {
+server.listen(server_port, server_ip_address, function () {
     console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
 });
