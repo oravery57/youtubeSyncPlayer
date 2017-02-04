b1aaf1964a1b4978ce73bcb0420e3e1cf9aa72a2
Yuki Ochi <yuki.ochi.oravery@gmail.com>
2017-02-04 17:48:45 +0900

    fix.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
+var http = require('http');
 var server = http.createServer();
 server.on('request', function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
