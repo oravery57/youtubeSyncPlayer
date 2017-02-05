f26b00e12fcd2ec27055d0ec5307a43635896af3
Yuki Ochi <yuki.ochi.oravery@gmail.com>
2017-02-05 23:15:46 +0900

    fix.

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ var fs = require( 'fs' ); // ファイル入出力モジュール読み込み
 
 var server = http.createServer();
 server.on('request', function(req, res) {
-    res.writeHead(200, { 'Content-Type' : 'text/html' });
-    res.end( fs.readFileSync('./index.html', 'utf-8') );
+    var url = req.url;
+    if ('/' == url) {
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
+        res.end( fs.readFileSync('./index.html', 'utf-8') );
+    } else if ('/js/youtube.js' == url) {
+        res.writeHead(200, { 'Content-Type' : 'text/plain' });
+        res.end( fs.readFileSync('./js/youtube.js', 'utf-8') );
+    } else if ('/css/login.css' == url) {
+        res.writeHead(200, { 'Content-Type' : 'text/css' });
+        res.end( fs.readFileSync('./css/login.css', 'utf-8') );
+    }
 });
 
 server.listen(server_port, server_ip_address, function () {
@@ -18,18 +27,57 @@ server.listen(server_port, server_ip_address, function () {
 // サーバーをソケットに紐付ける
 var io = socketio.listen(server);
 
+var store = [];
+
 // 接続確立後の通信処理部分を定義
-io.sockets.on( 'connection', function( socket ) {
+var socket = io.sockets.on( 'connection', function( client ) {
 
-    // クライアントからサーバーへ メッセージ送信ハンドラ（自分を含む全員宛に送る）
-    socket.on( 'c2s_message', function( data ) {
-        // サーバーからクライアントへ メッセージを送り返し
-        io.sockets.emit( 's2c_message', { value : data.value } );
+    client.emit('connected');
+
+    client.on('disconnect', function() {
+        var name = store[client.id].name;
+        var room = store[client.id].room;
+        delete store [client.id];
+        io.to(room).emit('message', {system: true, name : 'System', message: '[' + name + '] has leaved!', time: getTime()});
+        io.to(room).emit('updateMember', getMembers(room));
     });
 
-    // クライアントからサーバーへ メッセージ送信ハンドラ（自分以外の全員宛に送る）
-    socket.on( 'c2s_broadcast', function( data ) {
-        // サーバーからクライアントへ メッセージを送り返し
-        socket.broadcast.emit( 's2c_message', { value : data.value } );
+    client.on('join', function(data) {
+        usrobj = {
+            'room': data.room,
+            'name': data.name
+        };
+        store[client.id] = usrobj;
+        client.join(data.room);
+        client.emit("join");
+        io.to(data.room).emit('message', {system: true, name : 'System', message: '[' + data.name + '] has joined!', time: getTime()});
+        io.to(data.room).emit('updateMember', getMembers(data.room));
+        // client.emit('System', '[' + data.name + '] has joined!');
+
+        // client.emit('initialized');
     });
+
+    client.on( 'play', function( data ) {
+        io.to(store[client.id].room).emit('message', {system: true, name : 'System', message: '[' + data.name + '] starts new video [' + data.id + ']!', time: getTime()});
+        io.to(store[client.id].room).emit( 'play', { id : data.id } );
+    });
+
+    client.on( 'message', function( data ) {
+        io.to(store[client.id].room).emit('message', {name : data.name, message: data.message, time: getTime()});
+    });
+
+
 });
+
+function getTime() {
+    return new Date().toISOString();
+}
+
+function getMembers(room)  {
+    // TODO
+    var members = [];
+    Object.keys(store).forEach(function(key) {
+        if (store[key].room === room) members.push(store[key].name);
+    });
+    return members;
+}
